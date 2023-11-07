e590b4464c0639a8f6133106f4ec7351c0bed430
a1adikk-07 <136816822+a1adikk-07@users.noreply.github.com>
2023-11-07 21:57:48 +0200

    task two

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+
+player.on('timeupdate', throttle(ontTimeUpdate, 1000));
+
+function ontTimeUpdate() {
+    player
+    .getCurrentTime()
+    .then(currentTime => localStorage.setItem(LOCALSTORAGE_KEY, currentTime));
+}
+
+
+player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY) ?? 0);
