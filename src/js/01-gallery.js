ee7e64e741d58e3a720df011905723e7bdea13e8
a1adikk-07 <136816822+a1adikk-07@users.noreply.github.com>
2023-11-07 13:37:28 +0200

    normal start hw8 js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
+
+const cont = document.querySelector(".gallery");
+cont.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+
+function createMarkup(arr) {
+  return arr
+    .map(({ preview, original, description }) => `
+<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
+    `)
+    .join("");
+}
+
+var lightbox = new SimpleLightbox(".gallery__item a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
